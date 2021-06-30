6c0e2103bcb1a0737e5dba15bb74e43db1fe6494
Zsolt Kovacs <kovacs.zsoltzx@gmail.com>
2021-06-30 13:22:35 +0200

    Implemented Material-UI

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,14 @@ import React from 'react'
 import App from '@/App'
 import { Provider } from 'react-redux'
 import { store } from '@/store'
+import { ThemeProvider } from '@material-ui/core'
+import { theme } from '@/themes/base.theme'
 
 ReactDOM.render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
+	<ThemeProvider theme={theme}>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</ThemeProvider>,
 	document.getElementById('root')
 )
